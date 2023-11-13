9f2ae812a41b280ac23ca82c67b081f97edf7344
wwwaas0 <127373635+wwwaas0@users.noreply.github.com>
2023-11-14 01:23:39 +0900

    feat : InputValidator 메뉴판에 없는 메뉴 입력시

--- src/main/java/christmas/model/Menu.java
+++ src/main/java/christmas/model/Menu.java
@@ -1,24 +1,26 @@
 package christmas.model;
 
 public enum Menu {
-    MUSHROOM_SOUP("애피타이저", 6000),
-    TAPAS("애피타이저", 5500),
-    CAESAR_SALAD("애피타이저",8000),
-    T_BONE_STEAK("메인",55000),
-    BBQ_RIB("메인",54000),
-    SEAFOOD_PASTA("메인",35000),
-    CHRISTMAS_PASTA("메인",25000),
-    CHOCOLATE_CAKE("디저트",15000),
-    ICE_CREAM("디저트",5000),
-    ZERO_COKE("음료",3000),
-    RED_WINE("음료",60000),
-    CHAMPAGNE("음료",25000);
+    MUSHROOM_SOUP("애피타이저", 6000, "양송이수프"),
+    TAPAS("애피타이저", 5500, "타파스"),
+    CAESAR_SALAD("애피타이저",8000, "시저샐러드"),
+    T_BONE_STEAK("메인",55000, "티본스테이크"),
+    BBQ_RIB("메인",54000, "바비큐립"),
+    SEAFOOD_PASTA("메인",35000, "해산물파스타"),
+    CHRISTMAS_PASTA("메인",25000, "크리스마스파스타"),
+    CHOCOLATE_CAKE("디저트",15000, "초코케이크"),
+    ICE_CREAM("디저트",5000, "아이스크림"),
+    ZERO_COKE("음료",3000, "제로콜라"),
+    RED_WINE("음료",60000, "레드와인"),
+    CHAMPAGNE("음료",25000, "샴페인");
 
     private final String category;
     private final int price;
+    private final String koreanName;
 
-    Menu(String category, int price) {
+    Menu(String category, int price, String koreanName) {
         this.category = category;
         this.price = price;
+        this.koreanName = koreanName;
     }
 }
--- src/main/java/christmas/validator/InputValidator.java
+++ src/main/java/christmas/validator/InputValidator.java
@@ -1,5 +1,10 @@
 package christmas.validator;
 
+import christmas.model.Menu;
+
+import java.util.EnumSet;
+import java.util.HashMap;
+
 public class InputValidator {
     //    식당 방문 날짜
 //    숫자 아닌 것 입력
@@ -20,6 +25,19 @@ public class InputValidator {
 
 //    주문할 메뉴 및 개수 입력
 //    메뉴판에 없는 메뉴 입력
+    public static void notOnTheMenu(HashMap<Menu, Integer> orders){
+        for (Menu menu : orders.keySet()) {
+            if (!menuExists(menu)) {
+                throw new IllegalArgumentException("메뉴판에 없는 메뉴가 주문되었습니다");
+            }
+        }
+    }
+
+    // 메뉴가 메뉴판에 있는지 확인
+    private static boolean menuExists(Menu menu) {
+        return EnumSet.allOf(Menu.class).contains(menu);
+    }
+
 //    메뉴 입력이 예시와 다름
 //    메뉴 개수가 1 이상이 아님
 
