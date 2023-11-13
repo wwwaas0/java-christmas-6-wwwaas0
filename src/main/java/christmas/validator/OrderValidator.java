b4baca50f13f9b4eb7d70287a7ab8441003d8e8b
wwwaas0 <127373635+wwwaas0@users.noreply.github.com>
2023-11-14 02:20:43 +0900

    feat : OrderValidator 메뉴 최대 개수

--- src/main/java/christmas/validator/OrderValidator.java
+++ src/main/java/christmas/validator/OrderValidator.java
@@ -9,15 +9,21 @@ public class OrderValidator {
     public static void isOnlyDrinks(HashMap<Menu, Integer> orders) {
         boolean othersWithoutDrinks = false;
         for (Menu menu : orders.keySet()) {
-            if(!menu.getCategory().equals("음료")){
+            if (!menu.getCategory().equals("음료")) {
                 othersWithoutDrinks = true;
             }
         }
-        if(othersWithoutDrinks == false){
+        if (othersWithoutDrinks == false) {
             throw new IllegalArgumentException("음료만 주문시 주문 불가합니다.");
         }
     }
-//    메뉴는 한번에 최대 20개까지만 주문 가능
+
+    //    메뉴는 한번에 최대 20개까지만 주문 가능
+    public static void maxOrder(HashMap<Menu, Integer> orders) {
+        if (orders.keySet().size() > 20) {
+            throw new IllegalArgumentException("메뉴는 한 번에 최대 20개까지만 주문 가능합니다.");
+        }
+    }
 
 //    총 주문 금액 10,000원 이상부터 이벤트(할인?) 적용
 }
