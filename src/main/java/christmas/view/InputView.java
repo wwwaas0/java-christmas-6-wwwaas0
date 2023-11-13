1a5c01f453960c8da4e67e8a0d31a144a21a6d8c
wwwaas0 <127373635+wwwaas0@users.noreply.github.com>
2023-11-14 00:52:21 +0900

    feat : InputView 예상 방문 날짜

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,7 +1,13 @@
 package christmas.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
 //    12월 중 식당 예상 방문 날짜 입력
+    public static String visitDate(){
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        return Console.readLine();
+    }
 
 //    주문할 메누, 개수 입력
 
