fa496a4c9176d785624179eb1a007076039608e0
wwwaas0 <127373635+wwwaas0@users.noreply.github.com>
2023-11-14 02:07:00 +0900

    feat : InputVaildator 메뉴 개수 예외처리

--- src/main/java/christmas/model/Order.java
+++ src/main/java/christmas/model/Order.java
@@ -1,5 +1,6 @@
 package christmas.model;
 
+import christmas.validator.InputValidator;
 import christmas.view.InputView;
 
 import java.util.HashMap;
@@ -15,13 +16,18 @@ public class Order {
     }
 
     public HashMap<Menu, Integer> orderProcessing(String orders){
-        List<String> orderItems = List.of(orders.split(","));
-        for(String item: orderItems){
-            List<String> menuAndNumber = List.of(item.split("-"));
-            Menu menu = getMenuByKoreanName(menuAndNumber.get(0));
-            this.orders.put(menu, Integer.parseInt(menuAndNumber.get(1)));
+        try{
+            List<String> orderItems = List.of(orders.split(","));
+            for(String item: orderItems){
+                List<String> menuAndNumber = List.of(item.split("-"));
+                Menu menu = getMenuByKoreanName(menuAndNumber.get(0));
+                this.orders.put(menu, Integer.parseInt(menuAndNumber.get(1)));
+            }
+            validate(this.orders);
+            return this.orders;
+        } catch(Exception e){
+            throw new IllegalArgumentException("알맞지 않은 주문 형식입니다.");
         }
-        return this.orders;
     }
 
     private static Menu getMenuByKoreanName(String koreanName) {
@@ -33,7 +39,7 @@ public class Order {
         return null;
     }
 
-    private void validate(){
-
+    private void validate(HashMap<Menu, Integer> orders){
+        InputValidator.notOnTheMenu(orders);
     }
 }
--- src/main/java/christmas/validator/InputValidator.java
+++ src/main/java/christmas/validator/InputValidator.java
@@ -2,6 +2,7 @@ package christmas.validator;
 
 import christmas.model.Menu;
 
+import java.util.Collection;
 import java.util.EnumSet;
 import java.util.HashMap;
 
@@ -23,9 +24,9 @@ public class InputValidator {
         }
     }
 
-//    주문할 메뉴 및 개수 입력
+    //    주문할 메뉴 및 개수 입력
 //    메뉴판에 없는 메뉴 입력
-    public static void notOnTheMenu(HashMap<Menu, Integer> orders){
+    public static void notOnTheMenu(HashMap<Menu, Integer> orders) {
         for (Menu menu : orders.keySet()) {
             if (!menuExists(menu)) {
                 throw new IllegalArgumentException("메뉴판에 없는 메뉴가 주문되었습니다");
@@ -38,10 +39,13 @@ public class InputValidator {
         return EnumSet.allOf(Menu.class).contains(menu);
     }
 
-//    메뉴 입력이 예시와 다름
-
-
-//    메뉴 개수가 1 이상이 아님
-
+    //    메뉴 개수가 1 이상이 아님
+    public static void moreThan1(Collection<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < 1) {
+                throw new IllegalArgumentException("메뉴의 개수가 1보다 적습니다.");
+            }
+        }
+    }
 
 }
