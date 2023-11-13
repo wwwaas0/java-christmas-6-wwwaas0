d978d6cd9eae327efc9c17b28dc1726eac20557f
wwwaas0 <127373635+wwwaas0@users.noreply.github.com>
2023-11-14 02:18:27 +0900

    feat : OrderValidator 음료만 주문하는 경우

--- src/main/java/christmas/validator/OrderValidator.java
+++ src/main/java/christmas/validator/OrderValidator.java
@@ -1,7 +1,23 @@
 package christmas.validator;
 
+import christmas.model.Menu;
+
+import java.util.HashMap;
+
 public class OrderValidator {
-//    음료만 주문시 주문 불가
+    //    음료만 주문시 주문 불가
+    public static void isOnlyDrinks(HashMap<Menu, Integer> orders) {
+        boolean othersWithoutDrinks = false;
+        for (Menu menu : orders.keySet()) {
+            if(!menu.getCategory().equals("음료")){
+                othersWithoutDrinks = true;
+            }
+        }
+        if(othersWithoutDrinks == false){
+            throw new IllegalArgumentException("음료만 주문시 주문 불가합니다.");
+        }
+    }
 //    메뉴는 한번에 최대 20개까지만 주문 가능
+
 //    총 주문 금액 10,000원 이상부터 이벤트(할인?) 적용
 }
