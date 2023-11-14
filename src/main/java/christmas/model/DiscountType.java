4b712e4e1c6231186b111aa556ca14a24963ee18
wwwaas0 <127373635+wwwaas0@users.noreply.github.com>
2023-11-14 21:55:35 +0900

    discount type에 한글 이름 추가

--- src/main/java/christmas/model/DiscountType.java
+++ src/main/java/christmas/model/DiscountType.java
@@ -1,17 +1,19 @@
 package christmas.model;
 
 public enum DiscountType {
-    CHRISTMAS_D_DAY(1000),
-    WEEKDAY(2023),
-    WEEKEND(2023),
-    SPECIAL(1000),
-    GIFT(25000),
-    NO_DISCOUNT(0);
+    CHRISTMAS_D_DAY(1000, "크리스마스 디데이 할인"),
+    WEEKDAY(2023, "평일 할인"),
+    WEEKEND(2023, "주말 할인"),
+    SPECIAL(1000, "특별 할인"),
+    GIFT(25000, "증정 이벤트"),
+    NO_DISCOUNT(0, "적용되는 할인 없음");
 
     private final int discountPrice;
+    private final String koreanName;
 
-    DiscountType(int discountPrice) {
+    DiscountType(int discountPrice, String koreanName) {
         this.discountPrice = discountPrice;
+        this.koreanName = koreanName;
     }
 
     public int getDiscountPrice() {
