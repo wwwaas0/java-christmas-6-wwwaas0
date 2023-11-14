9f47aac64733731fe460f08a4c8f8c394f4e6921
wwwaas0 <127373635+wwwaas0@users.noreply.github.com>
2023-11-14 18:10:00 +0900

    feat : DiscountController 할인 전 전체 주문 금액

--- src/main/java/christmas/controller/DiscountController.java
+++ src/main/java/christmas/controller/DiscountController.java
@@ -1,25 +1,33 @@
 package christmas.controller;
 
+import christmas.model.Menu;
+
+import java.util.HashMap;
+
 public class DiscountController {
     private static InputController inputController;
     private static int visiteDate;
 //    start 메소드
 
-//  get할인 전 총 주문 금액 구하기
+    //  get할인 전 총 주문 금액 구하기
     /*
     input controller에서 주문 목록 받아와서 총 주문 금액 계산
      */
+    public int getTotalPayment(HashMap<Menu, Integer> orders) {
+        int total = 0;
+        for (Menu menu : orders.keySet()) {
+            int price = menu.getPrice();
+            int number = orders.get(menu);
+            total += price * number;
+        }
+        return total;
+    }
 
-//    get증정 메뉴 구하기
-    /*
-    할인 전 총 주문 금액이 12만 원 이상이면, 샴페인 1개 증정
-    boolean으로 만들어서 증정 메뉴 있는지 없는지만 반환
-     */
 
 //    get 해당되는 할인 내역 구하기
     /*
     input controller에서 크리스마스 디데이 추가 할인 금액 받아오기
-    Discount 클래스에서 방문 날짜 주고 어떤 할인이 적용되는지 리스트로 받아오기
+    Discount 클래스에서 방문 날짜, 할인 전 총 주문 금액 주고 어떤 할인이 적용되는지 리스트로 받아오기
      */
 
 //    get 총 혜택 금액 구하기
