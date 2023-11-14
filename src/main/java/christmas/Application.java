e610fdfc7f322615c72770cc32c80cff631c86aa
wwwaas0 <127373635+wwwaas0@users.noreply.github.com>
2023-11-14 19:15:59 +0900

    feat: application class에 할인 컨트롤러의 start 메소드

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,11 @@
 package christmas;
 
+import christmas.controller.DiscountController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        DiscountController discountController = new DiscountController();
+        discountController.start();
     }
 }
