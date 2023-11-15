7f689c1558877cc40642e8c412c24df731767461
wwwaas0 <127373635+wwwaas0@users.noreply.github.com>
2023-11-15 21:32:53 +0900

    test: 알맞은 주문 내역 확인

--- src/test/java/christmas/model/OrderTest.java
+++ src/test/java/christmas/model/OrderTest.java
@@ -9,9 +9,26 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class OrderTest {
-    //주문 내역 출력
-    //할인 전 총 주문 금액
+    @DisplayName("알맞은 주문 내역을 출력한다.")
+    @Test
+    void 주문_내역_테스트(){
+        String orders = "티본스테이크-1,타파스-1,초코케이크-1,제로콜라-2";
+
+        assertThatThrownBy(() -> new Order(orders))
+                .hasMessageContaining("티본스테이크 1개")
+                .hasMessageContaining("타파스 1개")
+                .hasMessageContaining("초코케이크 1개")
+                .hasMessageContaining("제로콜라 2개");
+    }
+
+    @DisplayName("알맞은 할인 전 총 주문 금액을 출력한다.")
+    @Test
+    void 할인_전_총_주문금액_테스트(){
+
+    }
 
 
 
