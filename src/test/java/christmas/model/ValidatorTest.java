6ab567fca29da5a65a878c19462341423dd02da0
wwwaas0 <127373635+wwwaas0@users.noreply.github.com>
2023-11-15 21:18:26 +0900

    test: ValidatorTest 20개 이상 주문한 경우

--- src/test/java/christmas/model/ValidatorTest.java
+++ src/test/java/christmas/model/ValidatorTest.java
@@ -60,8 +60,12 @@ public class ValidatorTest {
     }
 
     //20개 넘는 메뉴 시킨 경우
+    @DisplayName("음료만 주문한 경우")
+    @Test
+    void 주문_20개_이상_테스트() {
+        String invalidOrder = "제로콜라-4,레드와인-2,티본스테이크-4,초코케이크-2,크리스마스파스타-2,해산물파스타-2,양송이수프-4,바비큐립-4";
 
-
-
-
+        assertThatThrownBy(() -> new Order(invalidOrder))
+                .hasMessageContaining("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    }
 }
